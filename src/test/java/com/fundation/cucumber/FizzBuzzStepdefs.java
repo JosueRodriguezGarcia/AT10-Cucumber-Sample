2d2adbfe5761737ba3c2f10d11ae9c9e4e90727d
JosueRodriguezGarcia <Josue.Rodriguez@fundacion-jala.org>
2019-08-07 18:18:37 -0400

    adding depedencies

---
+++ src/test/java/com/fundation/cucumber/FizzBuzzStepdefs.java
@@ -0,0 +1,19 @@
+package com.fundation.cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class FizzBuzzStepdefs {
+    @Given("Create a FizzBuzz game play")
+    public void createAFizzBuzzGamePlay() {
+    }
+
+    @When("I play with number {int}")
+    public void iPlayWithNumber(int arg0) {
+    }
+
+    @Then("The result was Fizz")
+    public void theResultWasFizz() {
+    }
+}
